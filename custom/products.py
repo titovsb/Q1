35bf0505ff7c7aa8d20530348876097e2c30c6de
Sergey Titov <titovsb@gmail.com>
2021-05-17 00:42:25 +0300

    Кейс ТОВАРЫ дополнен вводом пользователя

--- custom/products.py
+++ custom/products.py
@@ -6,29 +6,50 @@
 
 LIST_OF_PROPERTIES = ['название', "цена", "количество", "ед."]
 
-products = [
-    (1, {'название': 'компьютер', 'цена': 20000, 'количество': 5, 'ед': 'шт.'}),
-    (2, {'название': 'принтер', 'цена': 6000, 'количество': 2, 'ед': 'шт.'}),
-    (3, {'название': 'сканер', 'цена': 1000, 'количество': 7, 'ед': 'шт.'})
-            ]
+# products = [
+#     (1, {'название': 'компьютер', 'цена': 20000, 'количество': 5, 'ед': 'шт.'}),
+#     (2, {'название': 'принтер', 'цена': 6000, 'количество': 2, 'ед': 'шт.'}),
+#     (3, {'название': 'сканер', 'цена': 1000, 'количество': 7, 'ед': 'шт.'})
+#             ]
 
-def fill_input():
-    pass
+products = []
+exitcode = True
 
-def parse_product(k, v, analytics_list):
-    if k in analytics_list.keys():
-        tmp = analytics_list[k]
-        tmp.append(v)
-        analytics_list[k] = list(set(tmp))
-    else:
-        analytics_list[k] = [v]
-    return analytics_list
-
+# стоит ли упрощать и использовать while True и break?
+while exitcode:
+    exitcode = True if input('Новый продукт? (д/н): ').upper() == "Д" else False
+    if exitcode:
+        my_dict = {}
+        for idx in LIST_OF_PROPERTIES:
+            my_dict[idx] = input(f'{idx}: ')
+        if len(products):
+            products.append((max(x[0] for x in products)+1, my_dict))
+        else:
+            products.append((1, my_dict))
 
 analytics = {}
 for record in products:
-    _, record = [*record]
-    for key, value in record.items():
-        parse_product(key, value, analytics)
-
+    _, prod_dict = [*record]
+    for key, value in prod_dict.items():
+        if key in analytics.keys():
+            tmp = analytics[key]
+            tmp.append(value)
+            analytics[key] = list(set(tmp))
+        else:
+            analytics[key] = [value]
 print(analytics)
+
+"""
+Новый продукт? (д/н): д
+название: Соль
+цена: 10
+количество: 100
+ед.: шт.
+Новый продукт? (д/н): д
+название: Сахар
+цена: 150
+количество: 80
+ед.: шт.
+Новый продукт? (д/н): н
+{'название': ['Соль', 'Сахар'], 'цена': ['150', '10'], 'количество': ['80', '100'], 'ед.': ['шт.']}
+"""
