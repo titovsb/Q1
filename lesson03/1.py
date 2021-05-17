70ca6a44e191d18d8fc66afd1d92a47dee8b6b04
Sergey Titov <titovsb@gmail.com>
2021-05-17 23:01:12 +0300

    1 is done

---
+++ lesson03/1.py
@@ -0,0 +1,19 @@
+"""
+Написать функцию num_translate(), переводящую числительные от 0 до 10 c
+английского на русский язык.
+"""
+
+def num_translate(num: str) -> str:
+    """Возвращает переод цифры от 'one' до 'ten' или None"""
+    LATTORUS_DICT = {
+    'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять',
+    'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять',
+    }
+    if num in LATTORUS_DICT:
+        return LATTORUS_DICT[num]
+
+print('Для окончания работы введите "0"')
+while True:
+    s = input('Число прописью: ')
+    if s == '0': break
+    print(num_translate(s))
