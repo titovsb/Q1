735b6621a2f6720b11f899e4e1516d6558cc70d8
Sergey Titov <titovsb@gmail.com>
2021-05-18 14:29:08 +0300

    Задания 1 и 2: было [one..ten], стало [zero..ten]

--- lesson03/2.py
+++ lesson03/2.py
@@ -6,15 +6,16 @@
 def num_translate(num: str) -> str:
     """Возвращает переод цифры от 'one' до 'ten' или None"""
     LATTORUS_DICT = {
-    'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять',
-    'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять',
+        'zero': 'ноль', 'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре',
+        'five': 'пять', 'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять',
+        'ten': 'десять',
     }
     tmp = num.lower()
     if tmp in LATTORUS_DICT:
         return LATTORUS_DICT[tmp] if num in LATTORUS_DICT else LATTORUS_DICT[tmp].capitalize()
 
-print('Для окончания работы введите "0"')
+print('Для окончания работы нажмите Enter')
 while True:
     s = input('Число прописью: ')
-    if s == '0': break
+    if s == '': break
     print(num_translate(s))
