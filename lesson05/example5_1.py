7dee03b825bf87cda335de4a31cac752c890e791
Sergey Titov <titovsb@gmail.com>
2021-05-27 11:46:15 +0300

    Ex5_1 modified

--- lesson05/example5_1.py
+++ lesson05/example5_1.py
@@ -9,7 +9,7 @@ src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 
 result = []
 for el in src:
-    if src.count(el) == 1 and not result.count(el):
+    if src.count(el) == 1:
         result.append(el)
 
 print(result)
