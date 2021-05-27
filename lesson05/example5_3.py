817313751d463126f23a0a14b3234fc949448548
Sergey Titov <titovsb@gmail.com>
2021-05-27 11:49:42 +0300

    Ex5_3 cleared text

--- lesson05/example5_3.py
+++ lesson05/example5_3.py
@@ -5,23 +5,12 @@ src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 result = [23, 1, 3, 10, 4, 11]
 """
 
+src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 
-def mygen(s:list) -> list:
-    result = []
-    for el in s:
-        if s.count(el) == 1 and not result.count(el):
-            result.append(el)
-            yield el
+f = filter(lambda x: src.count(x) == 1, src)
 
-
-if __name__ == '__main__':
-
-    src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
-
-    f = filter(lambda x: src.count(x) == 1, src)
-
-    print(type(f), f)
-    print(list(f))
+print(type(f), f)
+print(list(f))
 
 
 '''
