73131139d1f319e8f4db07420acb8b0d32cbd7a2
Sergey Titov <titovsb@gmail.com>
2021-05-27 11:42:56 +0300

    Ex5 finished in 3 releases

--- lesson05/example5_3.py
+++ lesson05/example5_3.py
@@ -7,19 +7,24 @@ result = [23, 1, 3, 10, 4, 11]
 
 
 def mygen(s:list) -> list:
+    result = []
     for el in s:
-        if s.count(el) == 1:
+        if s.count(el) == 1 and not result.count(el):
+            result.append(el)
             yield el
 
 
 if __name__ == '__main__':
+
     src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
-    g = mygen(src)
-    print(type(g), g)
-    print(list(g))
+
+    f = filter(lambda x: src.count(x) == 1, src)
+
+    print(type(f), f)
+    print(list(f))
 
 
 '''
-<class 'generator'> <generator object mygen at 0x7f4c6bb38dd0>
+<class 'filter'> <filter object at 0x7f4118fb8fa0>
 [23, 1, 3, 10, 4, 11]
 '''
