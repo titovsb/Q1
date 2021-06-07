f295e1d801e70998751a13fed3dc97d2a4ae391c
Sergey Titov <titovsb@gmail.com>
2021-06-07 19:22:07 +0300

    Ex3_2 kwargs wrapper

--- lesson08/t3_2.py
+++ lesson08/t3_2.py
@@ -1,17 +1,18 @@
 """
-Декоратор в базе. маскировку и прочее в файле t3_1
+Декоратор в базе. именованные аргументы
 """
 
 def type_logger(func):
-    def logger(*args):
-        print(', '.join([f'{func.__name__}({x}: {type(x)})' for x in args]))
-        return func(*args)
+    def logger(**kwargs):
+        print(', '.join([f'{func.__name__}({k}: {v} {type(v)})' for k,v in kwargs.items()]))
+        return func(**kwargs)
     return logger
 
 @type_logger
-def calc_cube(*args):
-    return [x**3 for x in args]
+def calc_cube(**kwargs):
+    return [v**3 for _,v in kwargs.items()]
 
 if __name__ == '__main__':
-    a = calc_cube(3,4,5,6)
-    print(a)
+    mydict = dict(zip(['a','b','c'],[2,3,4]))
+    a = calc_cube(**mydict)
+    print(*a)
